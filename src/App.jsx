0219c0ebac71b1657a8639bc4cacf36b4c11ee31
HtunLinnKyaw <tunlinkyaw4781@gmail.com>
2021-11-01 18:12:54 +0630

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './styles/root.scss';
 const App = () => {
   return (
     <div className="app">
-      <h1>TIC TAC TOE</h1>
+      <h1>TIC TAC TOE Games</h1>
       <Board />
     </div>
   );
